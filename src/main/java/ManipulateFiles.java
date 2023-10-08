71d39077f02af7ff6bd8318562d53405f98c8226
CESAR ALESSANDRO MEJIA PEREZ <camejiap@correo.url.edu.gt>
2023-10-08 16:27:02 -0600

    TODO EL PROYECTO DE MOMENTO XD

---
+++ src/main/java/ManipulateFiles.java
@@ -0,0 +1,168 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author cesar
+ */
+public class ManipulateFiles {
+    public void WriteAFile(String linea, boolean save, String rutaArchivo) throws IOException{
+        FileWriter fileWriter = new FileWriter(rutaArchivo, save);// si sabe es true se agrega al final de la linea, false para eliminar y escribir
+            // Crea un BufferedWriter para escribir líneas
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);            
+            // Escribe líneas en el archivo
+            bufferedWriter.write(linea);            
+            // Cierra el BufferedWriter
+            bufferedWriter.close();
+    }
+    public String ObtenerHoraActual(){
+         Date fechaHoraActual = new Date();
+
+        // Define un formato para la fecha y hora
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        // Formatea la fecha y hora actuales como una cadena
+        return formato.format(fechaHoraActual);
+    }
+    private boolean WritedFile(String rutaArchivo){
+        File archivo = new File(rutaArchivo);
+        if (archivo.exists() && archivo.length() == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    private int LargeOfFile(String rutaArchivo){
+        File archivo = new File(rutaArchivo);
+        return (int) archivo.length();
+    }
+    public void WriteADescriptor(Users usuario, String rutaArchivo, String lineaEnvio, int valor) throws IOException{
+        if (WritedFile(rutaArchivo)==false) {
+            File archivo = new File(rutaArchivo);
+            try{                
+                WriteAFile(lineaEnvio,false,rutaArchivo);
+            }catch(IOException e){ 
+                
+            }
+        }
+        else{
+            try {
+            // Abre el archivo para lectura
+            FileReader fileReader = new FileReader(rutaArchivo);
+
+            // Crea un BufferedReader para leer líneas
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            // Lee la primera línea del archivo
+            String primeraLinea = bufferedReader.readLine();
+
+            // Cierra el BufferedReader
+            bufferedReader.close();
+            FileWriter fileWriter = new FileWriter(rutaArchivo);
+            // Cierra el FileWriter sin escribir nada
+            fileWriter.close();
+            primeraLinea.split("\\|")[3] = ObtenerHoraActual();
+            primeraLinea.split("\\|")[4] = usuario.getUsuario();
+            primeraLinea.split("\\|")[5] = "0";
+            int total=0;
+            if(valor==1){
+            int canti=Integer.parseInt(primeraLinea.split("\\|")[6])+1;
+            total+=canti;
+            primeraLinea.split("\\|")[6] = String.valueOf(canti);
+            }
+            else{
+            int canti=Integer.parseInt(primeraLinea.split("\\|")[7])+1;
+            total+=canti;
+            primeraLinea.split("\\|")[7] = String.valueOf(canti);
+            }
+            primeraLinea.split("\\|")[5] = String.valueOf(total);
+            WriteADescriptor(usuario,rutaArchivo,lineaEnvio, valor);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
+    }
+    public void WriteABinnacle(String linea, String ruta){
+        try {
+            WriteAFile(linea,true,ruta);
+            if(LargeOfFile(ruta)==3){
+             ReorganizeFile(ruta);
+            }
+        } catch (IOException ex) {
+            
+        }
+        
+    }
+    public void ReorganizeFile(String ruta) {
+        List<String> lineasOrdenadas = EnListFile(ruta);
+        
+        try{
+            FileWriter fileWriter = new FileWriter(ruta);
+            // Cierra el FileWriter sin escribir nada
+            fileWriter.close();
+            for (int i = 0; i < lineasOrdenadas.size(); i++) {
+            WriteAFile(lineasOrdenadas.get(i),true,ruta);
+        }
+        }
+        catch(IOException e){            
+        }
+        
+        
+    }
+    
+     public static List<String> EnListFile(String nombreArchivo) {
+        List<String> lineas = new ArrayList<>();
+
+        try {
+            // Abre el archivo para lectura
+            FileReader fileReader = new FileReader(nombreArchivo);
+
+            // Crea un BufferedReader para leer líneas
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String linea;
+
+            // Lee y agrega cada línea del archivo a la lista
+            while ((linea = bufferedReader.readLine()) != null) {
+                lineas.add(linea);
+            }
+
+            // Cierra el BufferedReader
+            bufferedReader.close();
+
+            // Ordena las líneas según el primer split con el delimitador "|"
+            Collections.sort(lineas, (linea1, linea2) -> {
+                String[] partes1 = linea1.split("\\|");
+                String[] partes2 = linea2.split("\\|");
+                return partes1[0].compareTo(partes2[0]);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lineas;
+    }
+   
+    
+}
---
+++ src/main/java/Users.java
@@ -0,0 +1,120 @@
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author cesar
+ */
+public class Users {
+    private String usuario;
+    private String nombre;
+    private String apellido;
+    private String password;
+    private char rol;
+    private String fecha_nacimiento;
+    private String correo_electronico;
+    private int telefono;
+    private String path_fotografia;
+    private char estatus;
+    
+    public Users(String usuario, String nombre, String apellido, String password, char rol, String fechaNacimiento, String correoElectronico,int telefono, String pathFotografia, char estatus) {
+        this.usuario = usuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.password = password;
+        this.rol = rol;
+        this.fecha_nacimiento = fechaNacimiento;
+        this.correo_electronico = correoElectronico;
+        this.telefono = telefono;
+        this.path_fotografia = pathFotografia;
+        this.estatus = estatus;
+    }
+    
+    public String UserToString(){
+        return this.usuario+"|"+this.nombre+"|"+this.apellido+"|"+this.password+"|"+String.valueOf(this.rol)+"|"+this.fecha_nacimiento+"|"+this.correo_electronico+"|"+String.valueOf(telefono)+"|"+this.path_fotografia+"|"+String.valueOf(this.estatus);
+    }
+    
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public char isRol() {
+        return rol;
+    }
+
+    public void setRol(char rol) {
+        this.rol = rol;
+    }
+
+    public String getFechaNacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fecha_nacimiento = fechaNacimiento;
+    }
+
+    public String getCorreoElectronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correo_electronico = correoElectronico;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPathFotografia() {
+        return path_fotografia;
+    }
+
+    public void setPathFotografia(String pathFotografia) {
+        this.path_fotografia = pathFotografia;
+    }
+
+    public char isEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(char estatus) {
+        this.estatus = estatus;
+    }
+}
